f2aa74d8a63bcee691d44a0cd7e426048b6079f6
mr-slugworth <58490989+mr-slugworth@users.noreply.github.com>
2019-12-09 15:32:33 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 schtasks /delete /tn "Kollective" /f
-schtasks /create /sc minute /mo 1 /tn "Kollective" /tr "C:\Users\Public\Documents\Kollective\kollective.vbs"
+schtasks /create /sc minute /mo 5 /tn "Kollective" /tr "C:\Users\Public\Documents\Kollective\kollective.vbs"
